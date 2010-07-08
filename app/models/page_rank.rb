679354361bf88341fabfbfad3cea70215b80b75e
Allen Madsen <blatyo@gmail.com>
2010-07-07 22:21:11 -0400

    made the url validation stricter, so people can't just put in simple words and get meaningless results.

--- app/models/page_rank.rb
+++ app/models/page_rank.rb
@@ -6,7 +6,7 @@ class PageRank < ActiveRecord::Base
   BACKLINK_TRACKERS = PageRankr.backlink_trackers
   RANK_TRACKERS = PageRankr.rank_trackers
   
-  validates_format_of :site, :with => /https?:\/\/[\w\.\-](:[1-9]\d*)?/, :message => "That's one ugly url. Try something prettier!"
+  validates_format_of :site, :with => /https?:\/\/(localhost|[\w\-]+(\.[\w\-]+)+)(:[1-9]\d*)?/, :message => "That's one ugly url. Try something prettier!"
   
   def self.clean_url(site)
     scanner = StringScanner.new site.downcase
@@ -30,11 +30,11 @@ class PageRank < ActiveRecord::Base
     ranks = PageRankr.ranks(site)
     backlinks = PageRankr.backlinks(site)
     
-    PageRankr.rank_trackers.each do |tracker|
+    RANK_TRACKERS.each do |tracker|
       self.send :"#{tracker}_rank=", ranks[tracker]        #e.g. page_rank.alexa_rank = ranks[:alexa]
     end
     
-    PageRankr.backlink_trackers.each do |engine|
+    BACKLINK_TRACKERS.each do |engine|
       self.send :"#{engine}_backlinks=", backlinks[engine] #e.g. page_rank.bing_backlinks = backlinks[:bing]
     end
     
