7c006000ea12db066ad23791d0a8d35c5a49dcf0
Allen Madsen <blatyo@gmail.com>
2011-10-09 20:58:12 -0400

    Remove old files

--- test/unit/helpers/ranks_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class RanksHelperTest < ActionView::TestCase
-end
--- test/unit/rank_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class RankTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
